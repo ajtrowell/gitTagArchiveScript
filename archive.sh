374df513c43904dc8626c55cadf9b7e27d3c2a0b
AJTrowell <ajtrowell@gmail.com>
2017-04-01 17:47:23 -0500

    Initial Commit for git tag archive script
    This bash script is intended to copy the contents of
    each tagged commit within a git repository to a
    folder named after the tag.
    If a folder already exists with the tag name, it should
    be skipped.

---
+++ archive.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# archive script, by Ashley Trowell
+# Copies tagged commits from a git repository to named folders.
+# Does not overwrite existing folders.
+# To make executable, run: chmod 755 archive.sh
+
+REPO_DIR=exampleRepo
+
+echo;
+echo Git Archive Script
+echo;
+
+# Find local directories, and store in DIRS array.
+echo Show Folders:
+echo;
+DIRS=(*/) # Store all directories into DIRS array.
+for dir in "${DIRS[@]}";
+do 
+    echo "$dir";
+done
+
+
+# Find Git Tags, and store in TAGS array.
+cd $REPO_DIR
+i=0;
+for tag in $(git tag)
+do
+    TAGS[$i]=$tag; # Create TAGS array.
+    ((i++))
+done
+
+
+
+# Capture HASHES for TAGS into ARRAY (Don't have a reason yet)
+echo
+echo Capture and show TAGS HASHES
+echo
+i=0;
+for tag in "${TAGS[@]}"
+do
+    HASHES[$i]=$(git rev-parse $tag)
+    echo "$i $tag ${HASHES[$i]}"
+    ((i++)) # Increment i
+done
+
+
+# Determine which TAGS are not in DIRS, and store in NEW_DIRS array.
+echo
+echo Capture and show NEW_DIRS
+echo
+cd ..
+i=0;
+for tag in "${TAGS[@]}"
+do
+    DIR_EXISTS=0; # Assume Tag Dir doesn't exist, until found.
+
+    if [ -d "$tag" ]  # If "tag" folder exists...
+    then
+        echo "The folder $tag exists."
+        DIR_EXISTS=1;
+    fi
+   
+    # If Tag didn't match any existing Dir, add to NEW_DIRS array.
+    if [ $DIR_EXISTS -eq "0" ]
+    then
+        echo "Adding $tag as directory in NEW_DIRS."
+        NEW_DIRS[$i]=$tag
+        ((i++)) # Increment i
+    fi
+done
+
+echo;echo "Display all New Directories to be created:"
+echo ${NEW_DIRS[@]}
+echo;
+
+
+# Create New Directories:
+for dir in ${NEW_DIRS[@]}
+do
+    echo "Creating Directory $dir"
+    mkdir $dir
+    cd $REPO_DIR
+    git checkout $dir
+    cd ..
+done
+
+
+cd $REPO_DIR
+git checkout master
